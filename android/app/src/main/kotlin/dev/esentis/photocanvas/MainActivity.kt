693f3bb5d8478fd4983410a4c5fd7147b829c0bf
esentis <esentakos@yahoo.gr>
2021-11-06 01:45:20 +0200

    🎉 Initial commit

---
+++ android/app/src/main/kotlin/dev/esentis/photocanvas/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.esentis.photocanvas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'dart:html' as html;
+import 'dart:typed_data';
+
+import 'package:drop_zone/drop_zone.dart';
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:palette_generator/palette_generator.dart';
+
+var log = Logger();
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color containerColor = Colors.red;
+
+  Uint8List? imageData;
+
+  Future<void> loadImage(html.File file) async {
+    final reader = html.FileReader();
+    reader.readAsArrayBuffer(file);
+    await reader.onLoad.first;
+    setState(() {
+      imageData = reader.result as Uint8List;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          children: [
+            DropZone(
+              onDragEnter: () {
+                print('drag enter');
+                setState(() {
+                  containerColor = Colors.green;
+                });
+              },
+              onDragExit: () {
+                print('drag exit');
+                setState(() {
+                  containerColor = Colors.red;
+                });
+              },
+              onDrop: (List<html.File>? files) async {
+                final file = files?[0];
+                await loadImage(file!);
+
+                final paletteGenerator =
+                    await PaletteGenerator.fromImageProvider(
+                  Image.memory(imageData!).image,
+                  maximumColorCount: 20,
+                );
+
+                log.wtf(
+                    '${paletteGenerator?.dominantColor?.color}\n${paletteGenerator?.lightVibrantColor?.color}');
+
+                // image = File(files?[0].relativePath ?? '');
+                setState(() {
+                  containerColor = Colors.red;
+                });
+              },
+              child: Container(
+                color: containerColor,
+                width: 300,
+                height: 300,
+              ),
+            ),
+            if (imageData != null) Image.memory(imageData!),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
