b9a5acb2f90c9c6b3401445ecc17e0c95bc0f3de
esentis <esentakos@yahoo.gr>
2021-11-06 15:21:34 +0200

    Progresses main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'dart:html' as html;
 import 'dart:typed_data';
+import 'dart:ui';
 
+import 'package:dotted_border/dotted_border.dart';
 import 'package:drop_zone/drop_zone.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 import 'package:palette_generator/palette_generator.dart';
+import 'package:photocanvas/widgets/circle_color.dart';
+import 'package:photocanvas/widgets/inner_shadow.dart';
 
-var log = Logger();
+import 'constants.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,46 +21,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Photocanvas',
+      home: MyHomePage(title: 'Photocanvas'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Color containerColor = Colors.red;
-
+  Color containerColor = kColorMain;
+  String containerText = 'Drop your image here';
+  String focusedColorHex = '';
+  List<Color> activeColors = [];
   Uint8List? imageData;
 
   Future<void> loadImage(html.File file) async {
@@ -64,67 +47,165 @@ class _MyHomePageState extends State<MyHomePage> {
     reader.readAsArrayBuffer(file);
     await reader.onLoad.first;
     setState(() {
-      imageData = reader.result as Uint8List;
+      imageData = reader.result! as Uint8List;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final mq = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: kColorBackground,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: kColorBackground,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(widget.title, style: kStyle),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  imageData = null;
+                  activeColors = [];
+                });
+              },
+              icon: Icon(Icons.restore),
+            )
+          ],
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+          mainAxisAlignment: imageData == null
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.spaceBetween,
           children: [
-            DropZone(
-              onDragEnter: () {
-                print('drag enter');
-                setState(() {
-                  containerColor = Colors.green;
-                });
-              },
-              onDragExit: () {
-                print('drag exit');
-                setState(() {
-                  containerColor = Colors.red;
-                });
-              },
-              onDrop: (List<html.File>? files) async {
-                final file = files?[0];
-                await loadImage(file!);
+            const SizedBox(
+              height: 20,
+            ),
+            if (imageData == null)
+              DropZone(
+                onDragEnter: () {
+                  setState(() {
+                    containerColor = kColorSuccess;
+                    containerText = 'Ready to drop';
+                  });
+                },
+                onDragExit: () {
+                  setState(() {
+                    containerColor = kColorMain;
+                    containerText = 'Drop your image here';
+                  });
+                },
+                onDrop: (List<html.File>? files) async {
+                  final file = files?[0];
+                  await loadImage(file!);
 
-                final paletteGenerator =
-                    await PaletteGenerator.fromImageProvider(
-                  Image.memory(imageData!).image,
-                  maximumColorCount: 20,
-                );
+                  final paletteGenerator =
+                      await PaletteGenerator.fromImageProvider(
+                    Image.memory(imageData!).image,
+                    maximumColorCount: 20,
+                  );
+                  activeColors = paletteGenerator.colors.toList();
+                  log.wtf(paletteGenerator.colors.length);
+                  for (final c in paletteGenerator.colors) {
+                    log.wtf(kColorToHexString(c));
+                  }
 
-                log.wtf(
-                    '${paletteGenerator?.dominantColor?.color}\n${paletteGenerator?.lightVibrantColor?.color}');
-
-                // image = File(files?[0].relativePath ?? '');
-                setState(() {
-                  containerColor = Colors.red;
-                });
-              },
-              child: Container(
-                color: containerColor,
-                width: 300,
-                height: 300,
+                  // image = File(files?[0].relativePath ?? '');
+                  setState(() {
+                    containerColor = kColorMain;
+                  });
+                },
+                child: DottedBorder(
+                  borderType: BorderType.RRect,
+                  strokeWidth: 2,
+                  radius: const Radius.circular(20),
+                  padding: const EdgeInsets.all(12),
+                  color: containerColor,
+                  child: InnerShadow(
+                    blur: 7,
+                    color: kColorInnerShadow,
+                    child: Container(
+                      width: mq.width * .6,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        color: containerColor,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          containerText,
+                          style: kStyle.copyWith(
+                            fontSize: 50,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            if (imageData != null)
+              Container(
+                width: 350,
+                height: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: Image.memory(imageData!).image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ClipRRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 7,
+                    sigmaY: 7,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xff864879).withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Wrap(
+                      children: [
+                        if (activeColors.isNotEmpty)
+                          ...activeColors.map(
+                            (c) => Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: CircleColor(
+                                color: c,
+                                onHover: () {
+                                  setState(() {
+                                    focusedColorHex = kColorToHexString(c);
+                                  });
+                                },
+                                onExit: () {
+                                  setState(() {
+                                    focusedColorHex = '';
+                                  });
+                                },
+                              ),
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
-            if (imageData != null) Image.memory(imageData!),
+            const SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
