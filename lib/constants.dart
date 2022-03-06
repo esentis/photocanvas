7cab4a5721c0e97443620b2192abf6c5d1178da5
esentis <esentakos@yahoo.gr>
2022-03-06 17:01:12 +0200

    📝 Documents contstants

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,17 +5,40 @@ import 'package:url_launcher/url_launcher.dart';
 
 Logger log = Logger();
 
+///```dart
+///Color(0xff1F1D36);
+///```
 Color kColorBackground = const Color(0xff1F1D36);
+
+///```dart
+///Color(0xffEEEBDD).withOpacity(0.2);
+///```
 Color kColorInnerShadow = const Color(0xffEEEBDD).withOpacity(0.2);
+
+///```dart
+///Color(0xff864879);
+///```
 Color kColorMain = const Color(0xff864879);
+
+///```dart
+///Color(0xff66DE93);
+///```
 Color kColorSuccess = const Color(0xff66DE93);
 
+/// Default test style of the app
+///
+/// ```dart
+/// fontFamily: 'Dongle',
+/// fontSize: 30.sp,
+/// height: 0.8,
+/// ```
 TextStyle kStyle = TextStyle(
   fontFamily: 'Dongle',
   fontSize: 30.sp,
   height: 0.8,
 );
 
+/// Returns the Hex code of the color.
 String kColorToHexString(Color color) {
   return color.value.toRadixString(16).substring(2, 8);
 }
