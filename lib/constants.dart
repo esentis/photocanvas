dc75b69a4452c3bfe83fe1b3218501de88ebcbb7
esentis <52862600+esentis@users.noreply.github.com>
2025-01-14 23:50:21 +0200

    🔧 Refactor kColorToHexString function to include alpha channel and optional hash sign

--- lib/constants.dart
+++ lib/constants.dart
@@ -47,9 +47,22 @@ TextStyle kStyle = const TextStyle(
   height: 0.8,
 );
 
-/// Returns the Hex code of the color.
-String kColorToHexString(Color color) {
-  return color.value.toRadixString(16).substring(2, 8);
+// /// Returns the Hex code of the color.
+// String kColorToHexString(Color color) {
+//   return color.value.toRadixString(16).substring(2, 8);
+// }
+
+String kColorToHexString(Color color, {bool leadingHashSign = false}) {
+  final alpha = (color.a * 255).round();
+  final red = (color.r * 255).round();
+  final green = (color.g * 255).round();
+  final blue = (color.b * 255).round();
+
+  return '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
 
 String kGithubLink = 'https://www.github.com/esentis';
